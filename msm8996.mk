4f8aa91f6735243fe98b540a9d498b3e0abeff26
shivatejapeddi <shivatejapeddi@gmail.com>
2018-09-11 15:22:25 +0000

    msm8996-common : move thermal config to vendor

--- msm8996.mk
+++ msm8996.mk
@@ -339,8 +339,8 @@ PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
 # Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf
 
 # USB
 PRODUCT_PACKAGES += \
