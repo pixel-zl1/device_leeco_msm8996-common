9f781b0f9615ffdb6de77762c9ff76088030ddb2
mosimchah <mosimchah@gmail.com>
2018-09-19 17:59:24 -0400

    update gnss

--- msm8996.mk
+++ msm8996.mk
@@ -191,6 +191,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
     android.hardware.gnss@1.0-service-qti \
+    android.hardware.gnss@1.0-service \
+    android.hardware.gnss@1.0-impl:64 \
     libgnss \
     libgnsspps \
     libcurl
